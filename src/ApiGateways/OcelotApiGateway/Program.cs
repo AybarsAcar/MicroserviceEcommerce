9f40c3fedffd71925cfaaf321e9ee3dc476063f4
Aybars Acar <aybars.dev@gmail.com>
2022-12-25 11:49:02 +1100

    OcelotApiGateway added

---
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -0,0 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
